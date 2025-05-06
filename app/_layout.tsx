356575d5f2b97ebdfdf0783d13658d3abeb3b3d2
ashiruhammed <hammedashiru89@gmail.com>
2025-05-06 22:02:27 +0100

    Update Layout component to configure Stack options. Added screenOptions to hide the header in the Stack navigation, enhancing the overall UI experience.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
+import { Stack } from 'expo-router';
 import { StatusBar } from 'react-native';
 import '../global.css';
-
-import { Stack } from 'expo-router';
 
 export default function Layout() {
   return (
     <>
       <StatusBar barStyle={'light-content'} />
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </>
   );
 }
